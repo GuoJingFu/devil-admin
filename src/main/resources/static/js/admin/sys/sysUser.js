341f9bf69e35c02187d4ae1262a2d793a2c49214
devil <562157205@qq.com>
2017-07-31 23:54:27 +0800

    表格导出

--- src/main/resources/static/js/admin/sys/sysUser.js
+++ src/main/resources/static/js/admin/sys/sysUser.js
@@ -6,16 +6,33 @@ $(function () {
                 {field: 'id',title: 'id',visible: false,align: 'center',valign: 'middle'},
                 {field: 'account',title: '账号',align: 'center',valign: 'middle'},
                 {field: 'name',title: '名称',align: 'center',valign: 'middle'},
-                {field: 'createdAt',title: '创建时间',align: 'center',valign: 'middle'},
-                {field: 'isDeleted',title: '是否删除',align: 'center',valign: 'middle'}
+                {field: 'createdAt',title: '创建时间',align: 'center',valign: 'middle',formatter: dateFormatter},
+                {field: 'isDeleted',title: '是否删除',align: 'center',valign: 'middle',formatter: deleteFormatter}
         ]
     });
 });
-//配置参数
+//配置请求参数
 function queryParams(params) {
     var temp = {
         pageSize: params.pageSize*1,   //页面大小
         pageStart: params.pageNumber*1 //页码
     };
     return JSON.stringify(temp);
+}
+//日期格式化
+function dateFormatter(value) {
+    if (value != null) {
+        return new Date(value).pattern("yyyy-MM-dd HH:mm:ss");
+    }
+    return '';
+}
+//删除位格式化
+function deleteFormatter(value) {
+    if (value == 0) {
+        return '否';
+    } else if (value == 1) {
+        return '<span style="color: #f90906; font-weight: bold">是</span>';
+    } else {
+        return '';
+    }
 }
